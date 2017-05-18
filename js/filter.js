ecbede717ca9c4a9b2fe10b5cdca9fa162170b67
cl <605875053@qq.com>
2017-05-18 23:32:42 +0800

    修改filter.js支持多个filter和多个参数

--- js/filter.js
+++ js/filter.js
@@ -6,13 +6,42 @@ var filter = (function(){
     var walker = document.createTreeWalker(body,NodeFilter.SHOW_TEXT|NodeFilter.SHOW_ATTRIBUTE,null,false);
     var node = walker.nextNode();
     var filter={
-        a:function(str){
-            return '￥' + str
+        a:function(str,icon){
+            return icon + str
+        },
+        b:function(str){
+            console.log('b');
+            return str + '*'
+        },
+        c:function(str){
+            console.log('c');
+            return str + '@'
         }
     };
-    var data = {
-        time:new Date()
-    };
+    var data = {};
+    function filterFn(fn,arg){
+        return filter[fn].apply(filter,arg)
+    }
+    function assignment(arr){
+        var content = arr.shift();
+
+        arr.forEach(function(items){
+
+            var _arr = items.split(':');
+            var _fn = _arr.shift();
+
+            _arr.unshift(content);
+
+            _arr = _arr.map(function(item){
+
+                return data[item] || item
+
+            });
+            content = filterFn(_fn,_arr);
+
+        });
+        return content
+    }
     window.addEventListener('load',function(){
         while(node !== null){
             console.log(node.nodeValue);
@@ -20,23 +49,31 @@ var filter = (function(){
                 var arr = node.nodeValue.replace(/[\${}]/g,'').split('|');
                 console.log(arr);
 
-                if(typeof filter[arr[1]] === 'function'){
-                    node.nodeValue = filter[arr[1]](data[arr[0]]);
-                }
-            }
+                node.nodeValue = assignment(arr)
 
+            }
             node = walker.nextNode()
         }
     });
 
-    return function(key,fn){
-        filter[key] = fn
+    return {
+        filter:function(key,fn){
+            filter[key] = fn
+        },
+        setData:function(key,variable){
+            data[key] = variable
+        }
     }
 }());
 
 
 
-filter('date',function(time){
+filter.filter('date',function(time){
 
     return time.getTime()
 });
+
+filter.filter('toCase',function(str){
+    return str.toLocaleUpperCase()
+});
+filter.setData('time',new Date);
